5935c29ba33585478970763429804beffda4a8c1
Dylan <23158061@student.uwa.edu.au>
2026-07-09 16:15:36 +0800

    Changed setup script to prioritise latest package versions

--- Scripts/setonix_setup.sh
+++ Scripts/setonix_setup.sh
@@ -14,10 +14,6 @@ python -m venv .partial_qec_venv
 # Load the virtual environment
 source .partial_qec_venv/bin/activate
 
-# load in pre-installed modules from setonix
-module load py-scipy/1.14.1
-module load py-mpi4py/4.0.1-py3.11.6
-
 # Install required packages
 pip install --upgrade pip
-pip install numpy==2.4.6 pennylane==0.45.0 pennylane-lightning==0.45.0 torch torchvision mpi4py
+pip install numpy==2.4.6 scipy==1.17.1 mpi4py==1.4.2 pennylane==0.45.0 pennylane-lightning==0.45.0 torch torchvision mpi4py
